5d0e1e79dc78d57b83130e31883dac2faab6d66e
V <samaramailboxmain@gmail.com>
2024-09-25 23:36:19 +0400

    const03
    - объявления констант;
    - вывод разных типов в fmt.printf

---
+++ 1_basics/03_const/03_const.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+const pi = 3.141
+const (
+	hello = "Привет"
+	e     = 2.718
+)
+
+const (
+	zero = iota
+	_    //пустая, переменая, пропуск iota
+	two
+	three // = 3
+)
+const (
+	_         = iota             //пропускам первое значение
+	KB uint64 = 1 << (10 * iota) // 1 << (10 * 1) = 1024
+	MB                           // 1 << (10 * 2) = 1048576
+)
+const (
+	//нетипизированная константа
+	year = 2017
+	//типизированная константа
+	yearTyped int = 2017
+)
+
+func main() {
+	var month int32 = 13
+	fmt.Println(month + year)
+
+	fmt.Printf("pi: %f", pi)
+	fmt.Println()
+
+	fmt.Printf("const hello: %s, const e: %f", hello, e)
+	fmt.Println()
+
+	fmt.Printf("zero: %d, two: %d, three %d", zero, two, three)
+	fmt.Println()
+
+	fmt.Printf("KB: %b, MB: %d", KB, MB)
+	fmt.Println()
+
+	fmt.Print(yearTyped)
+	fmt.Println()
+}
