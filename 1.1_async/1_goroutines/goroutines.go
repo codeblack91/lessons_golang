c44ec7a778362296cbe1eda804678bb53cbd9c38
V <samaramailboxmain@gmail.com>
2024-10-08 17:45:07 +0400

    gorroutines1
    - введение в горутины;
    - WaitGroup.Add, Done, Wait;
    - GOMAXPROCS

---
+++ 1.1_async/1_goroutines/goroutines.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	iterationsnum = 7
+	goroutinesNum = 7
+)
+
+func main() {
+	// memory i[7]
+	for i := 0; i < goroutinesNum; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
+	}
+	fmt.Scanln()
+}
